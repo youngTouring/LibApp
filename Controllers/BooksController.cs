9e04b461bbbed0ee93bcf2ad13c8db028bfef4e8
pajakwsei <mpajak@wsei.edu.pl>
2021-10-30 15:45:55 +0200

    Added Index for Book model

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -34,25 +34,26 @@ namespace LibApp.Controllers
             return Content("id=" + bookId);
         }
 
-        public IActionResult Index(int? pageIndex, string sortBy) 
+        public IActionResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrEmpty(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+            var books = GetBooks();
+            
+            return View(books);
         }
         
         [Route("books/released/{year:regex(^\\d{{4}}$)}/{month:range(1, 12)}")]
         public IActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book {Id = 1, Name = "Hamlet"},
+                new Book {Id = 2, Name = "Ulysses"}
+            };
         }
     }
 }
