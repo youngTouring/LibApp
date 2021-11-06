14002c0e14a3129868500f9d3d8fc01ff920b227
pajakwsei <mpajak@wsei.edu.pl>
2021-11-06 21:11:06 +0100

    added class for initializing MembershipTypes table with data

---
+++ Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using LibApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.MembershipTypes.Any())
+                {
+                    Console.WriteLine("Database already seeded");
+                    return;
+                }
+
+                context.MembershipTypes.AddRange(
+                    new MembershipType
+                    {
+                        Id = 1,
+                        SignUpFee = 0,
+                        DurationInMonths = 0,
+                        DiscountRate = 0
+                    },
+                    new MembershipType
+                    {
+                        Id = 2,
+                        SignUpFee = 30,
+                        DurationInMonths = 1,
+                        DiscountRate = 10
+                    },
+                    new MembershipType
+                    {
+                        Id = 3,
+                        SignUpFee = 90,
+                        DurationInMonths = 3,
+                        DiscountRate = 15
+                    },
+                    new MembershipType
+                    {
+                        Id = 4,
+                        SignUpFee = 300,
+                        DurationInMonths = 12,
+                        DiscountRate = 20
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LibApp
 {
@@ -13,7 +15,16 @@ namespace LibApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
