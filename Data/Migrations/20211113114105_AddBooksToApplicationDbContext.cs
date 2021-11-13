deb95c9535948ae30e6844b622db31f8a1fd7ad6
Marcin Pająk <mpajak@wsei.edu.pl>
2021-11-13 12:48:30 +0100

    Added Details view for Books model and added Genre model

---
+++ Data/Migrations/20211113114105_AddBooksToApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp.Data.Migrations
+{
+    public partial class AddBooksToApplicationDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(type: "tinyint", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    AuthorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<byte>(type: "tinyint", nullable: false),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberInStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Genre_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,17 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-    }
+		[Required]
+		[StringLength(255)]
+		public string Name { get; set; }
+		[Required]
+		public string AuthorName { get; set; }
+		[Required]
+		public Genre Genre { get; set; }
+		public byte GenreId { get; set; }
+		public DateTime DateAdded { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public int NumberInStock { get; set; }
+	}
       
 }
