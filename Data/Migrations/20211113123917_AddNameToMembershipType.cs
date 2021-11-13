5915d273209021703da0ca11d3d389d767148435
Marcin Pająk <mpajak@wsei.edu.pl>
2021-11-13 13:45:01 +0100

    Updated the names of MembershipTypes

---
+++ Data/Migrations/20211113123917_AddNameToMembershipType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp.Data.Migrations
+{
+    public partial class AddNameToMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipTypes",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipTypes");
+        }
+    }
+}
