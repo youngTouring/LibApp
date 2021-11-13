707b5aa70200514cfe64d57309f71e31652bd034
Marcin Pająk <mpajak@wsei.edu.pl>
2021-11-13 11:55:44 +0100

    Modified CustomersController to get the Customers data from the database

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LibApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool HasNewsletterSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
