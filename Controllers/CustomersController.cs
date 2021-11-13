ff989509029b56dbe55d52ff4dbe897dc4711461
Marcin Pająk <mpajak@wsei.edu.pl>
2021-11-13 15:15:49 +0100

    Modified CustomersController to fetch data from DB and updated MembershipType model

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,21 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using LibApp.Models;
 using LibApp.ViewModels;
+using LibApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibApp.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList();
             
             return View(customers);
         }
 
         public IActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -27,15 +38,6 @@ namespace LibApp.Controllers
             }
 
             return View(customer);
-        }
-        
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Jan Kowalski" },
-                new Customer { Id = 2, Name = "Monika Nowak" }
-            };
         }
     }
 }
--- Data/Migrations/20211113134511_ApplyAnnotationsToCustomerName.Designer.cs
+++ Data/Migrations/20211113134511_ApplyAnnotationsToCustomerName.Designer.cs
@@ -4,14 +4,16 @@ using LibApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211113134511_ApplyAnnotationsToCustomerName")]
+    partial class ApplyAnnotationsToCustomerName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -54,11 +56,6 @@ namespace LibApp.Data.Migrations
 
                     b.Property<byte>("DurationInMonths")
                         .HasColumnType("tinyint");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasMaxLength(255)
-                        .HasColumnType("nvarchar(255)");
 
                     b.Property<short>("SignUpFee")
                         .HasColumnType("smallint");
