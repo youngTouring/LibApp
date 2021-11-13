a876a99411b2fd6c48641fd5865a4eba3b594a24
Marcin Pająk <mpajak@wsei.edu.pl>
2021-11-13 16:01:10 +0100

    Added Books to Database, prepared Index and Details views for Book model

---
+++ Data/Migrations/20211113145029_SetNamesOfGenres.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp.Data.Migrations
+{
+    public partial class SetNamesOfGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (1, 'Action')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (2, 'Thriller')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (3, 'Romance')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (4, 'Comedy')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (5, 'Report')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (6, 'Fantasy')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (7, 'SciFi')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
