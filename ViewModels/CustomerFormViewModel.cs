b7ce80e0b08c8f6bb56e78cd959316087e165466
Mikołaj Daraż <mikolajdaraz@gmail.com>
2021-12-12 14:45:35 +0100

    Modified CustomerFormViewModel

--- ViewModels/CustomerFormViewModel.cs
+++ ViewModels/CustomerFormViewModel.cs
@@ -1,6 +1,7 @@
 using LibApp.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,39 @@ namespace LibApp.ViewModels
 {
     public class CustomerFormViewModel
     {
-        public Customer Customer { get; set; }
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "Please provide customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool HasNewsletterSubscribed { get; set; }
+        [Display(Name = "Membership Type")]
+        [Required(ErrorMessage = "Please provide membership type")]
+        public byte? MembershipTypeId { get; set; }
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
+        public DateTime? Birthdate { get; set; }
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
 
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Edit customer" : "New customer";
+            }
+        }
+
+        public CustomerFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public CustomerFormViewModel(Customer customer)
+        {
+            Id = customer.Id;
+            Name = customer.Name;
+            Birthdate = customer.Birthdate;
+            MembershipTypeId = customer.MembershipTypeId;
+            HasNewsletterSubscribed = customer.HasNewsletterSubscribed;
+        }
     }
 }
