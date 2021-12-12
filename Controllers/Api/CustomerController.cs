396dd4ac44dee57686cf316501eecffdd280ed6d
Mikołaj Daraż <mikolajdaraz@gmail.com>
2021-12-12 16:19:33 +0100

    Added Api controller for CustomerModel

---
+++ Controllers/Api/CustomerController.cs
@@ -0,0 +1,101 @@
+using LibApp.Data;
+using LibApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HttpDeleteAttribute = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CustomersController : ControllerBase
+    {
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET /api/customers
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/customers/{id}
+        [HttpGet("{id}")]
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return customer;
+        }
+
+        // POST /api/customers
+
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/customers/{id}
+
+        [HttpPut("{id}")]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            var customerIndDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            customerIndDb.Name = customer.Name;
+            customerIndDb.Birthdate = customer.Birthdate;
+            customerIndDb.MembershipTypeId = customer.MembershipTypeId;
+            customerIndDb.HasNewsletterSubscribed = customer.HasNewsletterSubscribed;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/customers/{id}
+
+        [HttpDelete("{id}")]
+        public void DeleteCustomer(int id)
+        {
+            var customerIndDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerIndDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customers.Remove(customerIndDb);
+            _context.SaveChanges();
+        }
+
+        private ApplicationDbContext _context;
+    }
+}
