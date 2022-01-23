0ba56703c5e658f4bc55c08ae9b45985824e9c2d
padzak <mrcn.pajak@gmail.com>
2022-01-23 11:49:30 +0100

    Added MVC controller for Rentals model

---
+++ Controllers/RentalsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibApp.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public IActionResult New()
+        {
+            return View();
+        }
+    }
+}
